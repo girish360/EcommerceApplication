b7e2e23edd01bafbdd5c50c650a2707c8836b7c6
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-14 01:05:08 +0300

    Section 2 Section 7 Order ve Orderline modeli oluşturuldu.

---
+++ EcommerceApplication/Models/OrderLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApplication.Models
+{
+    [Table("OrderLine")]
+    public class OrderLine
+    {
+        public int OrderLineId { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? Price { get; set; }
+        public int? OrderId { get; set; }
+        public int? ProductId { get; set; }
+
+        public virtual Order Orders { get; set; }
+        public virtual Product Products { get; set; }
+    }
+}
