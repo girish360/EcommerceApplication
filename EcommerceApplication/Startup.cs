746ccbd1ec95073f64dc7047ee039e6f3bfa41a5
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-14 01:41:23 +0300

    Section 2 Lecture 12 Startup a Connection string bağımlılığı eklendi.

--- EcommerceApplication/Startup.cs
+++ EcommerceApplication/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using EcommerceApplication.DataContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceApplication
 {
@@ -29,6 +31,8 @@ namespace EcommerceApplication
         {
             // Add framework services.
             services.AddMvc();
+            services.AddDbContext<MyContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
