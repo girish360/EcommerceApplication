3faab7fe67f9a3ecce03f027c2b53675cc558a51
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-14 02:36:28 +0300

    Section 3 Lecture 18 SubCategory Repository Tamamlandı.

---
+++ EcommerceApplication/Services/Repository/SubCategoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using EcommerceApplication.Services.Infastracture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceApplication.Models;
+using EcommerceApplication.DataContext;
+
+namespace EcommerceApplication.Services.Repository
+{
+    public class SubCategoryRepository : ISubCategory
+    {
+        private MyContext _db;
+
+        public SubCategoryRepository(MyContext db)
+        {
+            _db = db;
+        }
+        public int Count()
+        {
+            return _db.SubCategory.Count();
+        }
+
+        public void Delete(int id)
+        {
+            var SubCategory = GetById(id);
+            if (SubCategory != null)
+            {
+                _db.SubCategory.Remove(SubCategory);
+            }
+        }
+
+        public IEnumerable<SubCategory> GetAll()
+        {
+            return _db.SubCategory.Select(c => c);
+        }
+
+        public SubCategory GetById(int id)
+        {
+            return _db.SubCategory.FirstOrDefault(c => c.SubCategoryId == id);
+        }
+
+        public void Insert(SubCategory subCat)
+        {
+            _db.SubCategory.Add(subCat);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(SubCategory subCat)
+        {
+            _db.SubCategory.Update(subCat);
+        }
+    }
+}
