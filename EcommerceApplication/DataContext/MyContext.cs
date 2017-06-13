e70171f2b7fbf5d5f6712b8ff8472b458562b028
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-14 01:23:14 +0300

    Section 2 Lecture 10 Data Context oluşturma Tamamlandı. Cart eklenmedi. Çünkü Cart sınıfı bir viewModel

---
+++ EcommerceApplication/DataContext/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using EcommerceApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApplication.DataContext
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<CartItem> CartItem { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderLine> OrderLine { get; set; }
+        public DbSet<Picture> Picture { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+
+    }
+}
