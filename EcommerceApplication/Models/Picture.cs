9ecdd1393161729fe85ed0d0a2a4804c939d1853
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-14 00:59:28 +0300

    Section 2 Lecture 6 Customer ve Picture modelleri eklendi.

---
+++ EcommerceApplication/Models/Picture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApplication.Models
+{
+    [Table("Picture")]
+    public class Picture
+    {
+        public Picture()
+        {
+            Products = new HashSet<Product>();
+        }
+        public int PictureId { get; set; }
+        public string FileName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
