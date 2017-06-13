32281f137383482acb7d524040c4568437ff5c51
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-14 00:50:26 +0300

    Section 2 Lecture 5 Model => Category- Product- SubCategory Oluşturuldu.

---
+++ EcommerceApplication/Models/SubCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceApplication.Models
+{
+    [Table("SubCategory")]
+    public class SubCategory
+    {
+        public int SubCategoryId { get; set; }
+        public string SubCategoryName { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
