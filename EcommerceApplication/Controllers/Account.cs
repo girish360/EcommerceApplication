df563ecdbad23f8865db5a4e115fec4fdab8e836
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-15 01:12:56 +0300

    section 5 lecture 40 tamamlandı. Acount Mvc controller class eklendi. identitycore sınıfı için usermanager, rolemanager, signmanager bağımlılıkları eklendi dependency Injection yapıldı.

---
+++ EcommerceApplication/Controllers/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using EcommerceApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EcommerceApplication.Controllers
+{
+    public class Account : Controller
+    {
+        private readonly UserManager<Customer> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly SignInManager<Customer> _signInManager;
+        public Account(UserManager<Customer> userManager,RoleManager<ApplicationRole> roleManager, SignInManager<Customer> signManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
