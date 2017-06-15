4cbf4a760dfd823f9ab68db49e7705d89d75c0d3
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-15 03:44:42 +0300

    section 5 lecture 48 eksik

--- EcommerceApplication/DataContext/MyContext.cs
+++ EcommerceApplication/DataContext/MyContext.cs
@@ -1,4 +1,5 @@
 ﻿using EcommerceApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,19 +10,24 @@ namespace EcommerceApplication.DataContext
 {
     public class MyContext:DbContext
     {
-        public MyContext(DbContextOptions<MyContext> options):base(options)
+        public class MyContext : IdentityDbContext<Customer, ApplicationRole, string>
         {
+            public MyContext(DbContextOptions<MyContext> options) : base(options)
+            {
 
+            }
+
+            public DbSet<CartItem> CartItem { get; set; }
+            public DbSet<Category> Category { get; set; }
+            public DbSet<Product> Product { get; set; }
+            public DbSet<Customer> Customer { get; set; }
+            public DbSet<Order> Order { get; set; }
+            public DbSet<OrderLine> OrderLine { get; set; }
+            public DbSet<Picture> Picture { get; set; }
+            public DbSet<SubCategory> SubCategory { get; set; }
+            public DbSet<ApplicationRole> ApplicationRole { get; set; }
         }
-
-        public DbSet<CartItem> CartItem { get; set; }
-        public DbSet<Category> Category { get; set; }
-        public DbSet<Product> Product { get; set; }
-        public DbSet<Customer> Customer { get; set; }
-        public DbSet<Order> Order { get; set; }
-        public DbSet<OrderLine> OrderLine { get; set; }
-        public DbSet<Picture> Picture { get; set; }
-        public DbSet<SubCategory> SubCategory { get; set; }
-
     }
+
 }
+
