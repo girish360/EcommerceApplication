c706cff8e5b56642f3fc77ab24f52d3a6acda0f7
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-16 04:32:50 +0300

    section 6 lecture 54 admin area daki product controller a bağımlılıklarımızı ekledik.

--- EcommerceApplication/Areas/Admin/Controllers/Products.cs
+++ EcommerceApplication/Areas/Admin/Controllers/Products.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EcommerceApplication.Services.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using EcommerceApplication.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +14,16 @@ namespace EcommerceApplication.Areas.Admin.Controllers
     [Area("Admin")]
     public class Products : Controller
     {
+        private readonly IProduct _productRepository;
+        private readonly ICategory _categoryRepository;
+        private readonly UserManager<Customer> _userManager;
+
+        public Products(IProduct productRepository, ICategory categoryRepository, UserManager<Customer> userManager)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _userManager = userManager;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
