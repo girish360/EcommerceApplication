a10e4637f7594d07be7fad1a107b1314cc7ca14e
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-16 04:34:44 +0300

    section 6 lecture 55 Admin area da bulunan category controller sınıfına bağımlılığımızı ekledik.

--- EcommerceApplication/Areas/Admin/Controllers/Category.cs
+++ EcommerceApplication/Areas/Admin/Controllers/Category.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EcommerceApplication.Services.Infrastructure;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,11 @@ namespace EcommerceApplication.Areas.Admin.Controllers
 {
     public class Category : Controller
     {
+        private readonly ICategory _categoryRepository;
+        public Category(ICategory categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
